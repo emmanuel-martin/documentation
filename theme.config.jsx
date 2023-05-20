d9692482cab9e5fb21d57e9b04a5885db3a95cc7
Emmanuel Martin <helloiamemmanuel@gmail.com>
2023-05-20 20:29:23 +0530

    configuration change
    -- added search
    -- folder structure
    -- removed test files

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,14 +1,13 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
 export default {
-    logo: <span>Emmanuel Martin.</span>,
-    docsRepositoryBase: 'https://github.com/emmanuel-martin/documentation/main/docs/pages',
-    project: {
-      link: 'https://github.com/emmanuel-martin/documentation'
-    },
-    breadcrumb: true,
-    search: true, 
-    footer: true,
-    darkMode: true,
-    footer: {
-        text: "A Project by Emmanuel Martin"
-    }
-  }
+  logo: <span><b>Documentation</b> | <span style={{fontSize: '12px'}}>Emmanuel Martin.</span></span>,
+  project: {
+    link: 'https://github.com/emmanuel-martin/documentation/tree/main/pages',
+  },
+  docsRepositoryBase: 'https://github.com/emmanuel-martin/documentation/tree/main/pages',
+  footer: {
+    text: 'A Project by Emmanuel Martin',
+  },
+}
