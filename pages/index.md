d69630243210b410417e41b9ff288d441e93303d
Emmanuel Martin <helloiamemmanuel@gmail.com>
2023-05-20 19:57:46 +0530

    basic structure

---
+++ pages/index.md
@@ -0,0 +1,3 @@
+# Welcome to Guides 
+
+Welcome to my site
---
+++ theme.config.jsx
@@ -0,0 +1,14 @@
+export default {
+    logo: <span>Emmanuel Martin.</span>,
+    docsRepositoryBase: 'https://github.com/emmanuel-martin/documentation/main/docs/pages'
+    project: {
+      link: 'https://github.com/emmanuel-martin/documentation'
+    },
+    breadcrumb: true,
+    search: true, 
+    footer: true,
+    darkMode: true,
+    footer: {
+        text: "A Project by Emmanuel Martin"
+    }
+  }
