b63fed91ddb88d93adf4d78367573a32538c6f86
Emmanuel Martin <helloiamemmanuel@gmail.com>
2023-05-20 21:25:18 +0530

    added sub folders

--- pages/index.md
+++ pages/index.md
@@ -1,3 +1,13 @@
 # Welcome to Guides 
 
-Welcome to my site
+Learn how to design great web apps.
+
+You'll be learning about 
+
+[Typography](/typography)
+
+## Credits
+
+## Blogs
+
+Also Checkout my blog site at [blog.emmanuel.codes](https://blog.emmanuel.codes)
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig, useConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
 
 export default {
   logo: <span><b>Documentation</b> | <span style={{fontSize: '12px'}}>Emmanuel Martin.</span></span>,
@@ -7,7 +8,33 @@ export default {
     link: 'https://github.com/emmanuel-martin/documentation/tree/main/pages',
   },
   docsRepositoryBase: 'https://github.com/emmanuel-martin/documentation/tree/main/pages',
+  useNextSeoProps() {
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Emmanuel Martin'
+      }
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://guide.emmanuel.codes' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+ 
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'Emmanuel Martin - Design Documentation'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Find documentation and resources for designing great apps.'}
+        />
+      </>
+    )
+  },
   footer: {
-    text: 'A Project by Emmanuel Martin',
+    text: "A project by Emmanuel Martin",
   },
 }
